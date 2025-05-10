2eb0b06b664afcc04237a01dd003e5ae3253390a
Amr-Mousa-333 <moamrmousa333@gmail.com>
2025-05-10 18:19:25 +0300

    Add files via upload

---
+++ project10.js
@@ -0,0 +1,16 @@
+// function comvermillesCel(l ,km ,milles , ft){
+//   if(km==km)
+//   {console.log(`${l} ${km}`)}
+//   else if(km=='mill')
+//     {console.log(`${l *1.6} ${milles}`)}
+//   else {
+//     {console.log(`${l /1.6} ${milles}`)}
+//   }
+//   }
+// comvermillesCel(32 ,'km','mill') ;
+
+let calculation = '';
+function fu1(n){
+  calculation += n ;
+  console.log(calculation);
+}
